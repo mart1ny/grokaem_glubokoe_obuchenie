b56d128beebd92ccd5cf125d4d3fbdffcdcfc51a
mart1ny <aurum.danil345@gmail.com>
2026-01-04 12:12:09 +0300

    Нейросетевая модель с импользованием numpy

---
+++ 3_глава/simple_neuro_with_np.py
@@ -0,0 +1,14 @@
+import numpy as np
+weight = np.array([0.1, 0.2, 0.0])
+def neural_network(weight, input):
+    prediction = np.dot(weight, input)
+    return prediction
+
+toes = np.array([8.5, 9.5, 9.9, 9.0])
+wins = np.array([0.65, 0.8, 0.8, 0.9])
+fans = np.array([1.2, 1.3, 0.5, 1.0])
+
+input = np.array([toes[0], wins[0], fans[0]])
+
+pred = neural_network(weight, input)
+print(pred)
